a705a29e3adfd9ffc9a7e626b0f1dc3f4383ef6e
Jeremy J. Miller <jm@chef.io>
2018-06-22 16:43:30 -0500

    fix solo.rb
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 node_name           "solo"
 log_level           :info
-log_locati          STDOUT
+log_location        STDOUT
 node_path           "/Users/jmiller/Devel/ChefProject/encrypted_data_bag_solo/nodes"
 cookbook_path       "/Users/jmiller/Devel/ChefProject/encrypted_data_bag_solo/cookbooks"
 data_bag_path       "/Users/jmiller/Devel/ChefProject/encrypted_data_bag_solo/data_bags"
